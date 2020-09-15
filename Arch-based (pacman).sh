aca34e7e6d0c9eddc4eb0dcb4f584aa876ff5481
Sidpatchy <35241490+Sidpatchy@users.noreply.github.com>
2020-09-14 21:38:41 -0500

    Update Arch-based (pacman).sh
    ngl, I hadn't used Arch when I originally made this

--- Arch-based (pacman).sh
+++ Arch-based (pacman).sh
@@ -7,22 +7,24 @@ sudo systemctl enable --now snapd.socket
 # Install yay
 sudo pacman -S --noconfirm yay
 
-sudo yay -S --noconfirm discord_arch_electron        # Discord
-sudo yay -S --noconfirm kdenlive                     # kdenlive
-sudo yay -S --noconfirm skanlite                     # Skanlite
-sudo yay -S --noconfirm onlyoffice-desktopeditors    # OnlyOffice
-sudo yay -S --noconfirm steam                        # Steam
-sudo yay -S --noconfirm termius-app                  # Termius SSH
-sudo yay -S --noconfirm visual-studio-code-bin       # Visual Studio Code
-sudo yay -S --noconfirm atom                         # Atom
-sudo yay -S --noconfirm spotify                      # Spotify
-sudo yay -S --noconfirm audacity                     # Audacity
-sudo yay -S --noconfirm chromium                     # Chromium (eww)
-sudo yay -S --noconfirm gimp                         # GIMP
-sudo yay -S --noconfirm vlc                          # VLC
-sudo yay -S --noconfirm qbittorrent                  # qBittorrent
-sudo yay -S --noconfirm install git                  # git
-sudo yay -S --no-confirm minecraft-launcher          # Minecraft
-sudo yay -S github-desktop                           # GitHub Desktop
+yay -S --noconfirm discord_arch_electron        # Discord
+yay -S --noconfirm kdenlive                     # kdenlive
+yay -S --noconfirm skanlite                     # Skanlite
+yay -S --noconfirm onlyoffice-desktopeditors    # OnlyOffice
+yay -S --noconfirm steam                        # Steam
+yay -S --noconfirm termius-app                  # Termius SSH
+yay -S --noconfirm visual-studio-code-bin       # Visual Studio Code
+yay -S --noconfirm atom                         # Atom
+yay -S --noconfirm spotify                      # Spotify
+yay -S --noconfirm audacity                     # Audacity
+yay -S --noconfirm chromium                     # Chromium (eww)
+yay -S --noconfirm gimp                         # GIMP
+yay -S --noconfirm vlc                          # VLC
+yay -S --noconfirm qbittorrent                  # qBittorrent
+yay -S --noconfirm install git                  # git
+yay -S --noconfirm minecraft-launcher           # Minecraft
+yay -S --noconfirm github-desktop               # GitHub Desktop
+yay -S --noconfirm teams                        # Microsoft Teams
+yay -S --noconfirm multimc5                     # MutliMC
 
 echo Done!
